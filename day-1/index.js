0e54000048de6852a37a83bc8afff9e9dc4a6880
jellythewobbly <jellythewobbly@hotmail.com>
2019-12-02 02:29:59 +0800

    add day 1 part 2

--- day-1/index.js
+++ day-1/index.js
@@ -9,5 +9,15 @@ var data = fs
     .split('\n')
     .map(Number);
 var calculateFuel = function (mass) { return ((mass / 3) | 0) - 2; };
-var totalFuel = data.reduce(function (ans, currentNum) { return ans + calculateFuel(currentNum); }, 0);
-console.log(totalFuel);
+var totalFuel = data.reduce(function (total, current) { return total + calculateFuel(current); }, 0);
+console.log("Part 1: " + totalFuel);
+var newTotalFuel = data.reduce(function (total, current) {
+    var fuelRequired = calculateFuel(current);
+    var additionalFuel = calculateFuel(fuelRequired);
+    while (additionalFuel > 0) {
+        fuelRequired += additionalFuel;
+        additionalFuel = calculateFuel(additionalFuel);
+    }
+    return total + fuelRequired;
+}, 0);
+console.log("Part 2: " + newTotalFuel);
--- day-1/index.ts
+++ day-1/index.ts
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const data = fs
+const data: number[] = fs
   .readFileSync('input.txt', (err, data) => {
     if (err) {
       throw err;
@@ -12,9 +12,23 @@ const data = fs
 
 const calculateFuel = (mass: number) => ((mass / 3) | 0) - 2;
 
-const totalFuel = data.reduce(
+const totalFuel: number = data.reduce(
   (total: number, current: number) => total + calculateFuel(current),
   0
 );
 
-console.log(totalFuel);
+console.log(`Part 1: ${totalFuel}`);
+
+const newTotalFuel: number = data.reduce((total: number, current: number) => {
+  let fuelRequired = calculateFuel(current);
+  let additionalFuel = calculateFuel(fuelRequired);
+
+  while (additionalFuel > 0) {
+    fuelRequired += additionalFuel;
+    additionalFuel = calculateFuel(additionalFuel);
+  }
+
+  return total + fuelRequired;
+}, 0);
+
+console.log(`Part 2: ${newTotalFuel}`);
