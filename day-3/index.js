d9a941c0e9c6dec4680dcf92a3f4bbc528b93658
jellythewobbly <jellythewobbly@hotmail.com>
2019-12-05 02:21:12 +0800

    add day 3 part 1

---
+++ day-3/index.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+var data = fs
+    .readFileSync('input.txt', function (err, data) {
+    if (err) {
+        throw err;
+    }
+})
+    .toString()
+    .split('\n');
+var wire1 = data[0].split(',');
+var wire2 = data[1].split(',');
+var MAP_X = { L: -1, R: 1, U: 0, D: 0 };
+var MAP_Y = { L: 0, R: 0, U: 1, D: -1 };
+var getPoints = function (wirePath) {
+    var x = 0;
+    var y = 0;
+    var length = 0;
+    var res = {};
+    for (var _i = 0, wirePath_1 = wirePath; _i < wirePath_1.length; _i++) {
+        var path = wirePath_1[_i];
+        var direction = path[0];
+        var distance = +path.slice(1);
+        for (var i = 0; i < distance; i++) {
+            x += MAP_X[direction];
+            y += MAP_Y[direction];
+            length += 1;
+            if (!res[x + "," + y]) {
+                res[x + "," + y] = length;
+            }
+        }
+    }
+    return res;
+};
+var points1 = getPoints(wire1);
+var points2 = getPoints(wire2);
+var both = {};
+for (var i in points1) {
+    if (points2[i]) {
+        both[i] = true;
+    }
+}
+var part1 = Math.min.apply(Math, Object.keys(both).map(function (point) {
+    return point.split(',').reduce(function (acc, cur) { return acc + Math.abs(+cur); }, 0);
+}));
+console.log("Part 1: " + part1);
+var part2 = Math.min.apply(Math, Object.keys(both).map(function (point) { return points1[point] + points2[point]; }));
+console.log("Part 2: " + part2);
---
+++ day-3/index.ts
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+const data: string[] = fs
+  .readFileSync('input.txt', (err, data) => {
+    if (err) {
+      throw err;
+    }
+  })
+  .toString()
+  .split('\n');
+
+const wire1: string[] = data[0].split(',');
+const wire2: string[] = data[1].split(',');
+
+const MAP_X = { L: -1, R: 1, U: 0, D: 0 };
+const MAP_Y = { L: 0, R: 0, U: 1, D: -1 };
+
+const getPoints = (wirePath: string[]) => {
+  let x = 0;
+  let y = 0;
+  let length = 0;
+
+  const res = {};
+
+  for (const path of wirePath) {
+    const direction = path[0];
+    const distance = +path.slice(1);
+
+    for (let i = 0; i < distance; i++) {
+      x += MAP_X[direction];
+      y += MAP_Y[direction];
+      length += 1;
+
+      if (!res[`${x},${y}`]) {
+        res[`${x},${y}`] = length;
+      }
+    }
+  }
+  return res;
+};
+
+const points1 = getPoints(wire1);
+const points2 = getPoints(wire2);
+
+const both = {};
+
+for (const i in points1) {
+  if (points2[i]) {
+    both[i] = true;
+  }
+}
+
+const part1: number = Math.min(
+  ...Object.keys(both).map(point =>
+    point.split(',').reduce((acc, cur) => acc + Math.abs(+cur), 0)
+  )
+);
+
+console.log(`Part 1: ${part1}`);
