e6c17dd9d13908283c2084dbcfad05b7eada837b
jellythewobbly <jellythewobbly@hotmail.com>
2019-12-16 23:34:19 +0800

    refactor day 6 part 1, add memo for optimisation

--- day-6/index.js
+++ day-6/index.js
@@ -7,9 +7,21 @@ const data = fs
     .split('\n');
 const map = {};
 data.forEach(element => {
-    const [parent, child] = element.split(')');
-    map[child] = parent;
+    if (element) {
+        const [parent, child] = element.split(')');
+        map[child] = parent;
+    }
 });
-const getOrbits = (key) => (map[key] ? 1 + getOrbits(map[key]) : 0);
+const getOrbits = (key) => {
+    if (typeof map[key] === 'number') {
+        return map[key];
+    }
+    if (map[key]) {
+        const res = 1 + getOrbits(map[key]);
+        map[key] = res;
+        return res;
+    }
+    return 0;
+};
 const part1 = Object.keys(map).reduce((sum, key) => sum + getOrbits(key), 0);
 console.log(`Part 1: ${part1}`);
--- day-6/index.ts
+++ day-6/index.ts
@@ -9,11 +9,23 @@ const data: string[] = fs
 const map = {};
 
 data.forEach(element => {
-  const [parent, child] = element.split(')');
-  map[child] = parent;
+  if (element) {
+    const [parent, child] = element.split(')');
+    map[child] = parent;
+  }
 });
 
-const getOrbits = (key: string) => (map[key] ? 1 + getOrbits(map[key]) : 0);
+const getOrbits = (key: string) => {
+  if (typeof map[key] === 'number') {
+    return map[key];
+  }
+  if (map[key]) {
+    const res = 1 + getOrbits(map[key]);
+    map[key] = res;
+    return res;
+  }
+  return 0;
+};
 
 const part1 = Object.keys(map).reduce(
   (sum: number, key: string) => sum + getOrbits(key),
