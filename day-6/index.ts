1dc0246988c9584e0a1f75e5a5c4f4b66c69ca90
jellythewobbly <jellythewobbly@hotmail.com>
2019-12-17 11:20:49 +0800

    add day 6 part 2, refactor part 1

--- day-6/index.ts
+++ day-6/index.ts
@@ -11,18 +11,18 @@ const map = {};
 data.forEach(element => {
   if (element) {
     const [parent, child] = element.split(')');
-    map[child] = parent;
+    map[child] = { parent };
   }
 });
 
 const getOrbits = (key: string) => {
-  if (typeof map[key] === 'number') {
-    return map[key];
-  }
-  if (map[key]) {
-    const res = 1 + getOrbits(map[key]);
-    map[key] = res;
-    return res;
+  if (key in map) {
+    if (map[key].orbitCount) {
+      return map[key].orbitCount;
+    }
+    const orbitCount = 1 + getOrbits(map[key].parent);
+    map[key].orbitCount = orbitCount;
+    return orbitCount;
   }
   return 0;
 };
@@ -33,3 +33,34 @@ const part1 = Object.keys(map).reduce(
 );
 
 console.log(`Part 1: ${part1}`);
+
+const santa = 'SAN';
+const you = 'YOU';
+
+const santaOrbits = getOrbits(santa);
+const youOrbits = getOrbits(you);
+const further = santaOrbits > youOrbits ? santa : you;
+const nearer = santaOrbits < youOrbits ? santa : you;
+
+let diff = Math.abs(santaOrbits - youOrbits);
+
+const getParentNode = (node: string) => map[node].parent;
+
+const furtherPointer = { current: getParentNode(further) };
+const nearerPointer = { current: getParentNode(nearer) };
+
+let orbitalTransfers = 0;
+
+while (furtherPointer.current !== nearerPointer.current) {
+  if (diff > 0) {
+    furtherPointer.current = getParentNode(furtherPointer.current);
+    diff -= 1;
+    orbitalTransfers += 1;
+  } else {
+    furtherPointer.current = getParentNode(furtherPointer.current);
+    nearerPointer.current = getParentNode(nearerPointer.current);
+    orbitalTransfers += 2;
+  }
+}
+
+console.log(`Part 2: ${orbitalTransfers}`);
